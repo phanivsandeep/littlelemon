efc086931de46a9421419f955c0749c405cdd11b
Chodavarapu Phani Visweswara Sandeep <phanivsandeep@gmail.com>
2025-04-07 21:16:03 -0400

    Adding assets and creating header,nav,main,footer components

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to the Main Component</h1>
+      <p>This is the main content area.</p>
+    </main>
+  );
+}
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+  return (
+    <nav>
+  <ul>
+    <li><a href="/home">Home</a></li>
+    <li><a href="/menu">Menu</a></li>
+    <li><a href="/reservations">Reservations</a></li>
+  </ul>
+</nav>
+  );
+}
+export default Nav;
